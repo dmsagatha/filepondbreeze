6aa23028d4ed106c482cc98fda6c87f733c1d7b5
Doris Morales <dmorales55@gmail.com>
2023-02-25 19:53:23 -0500

    Post: Imagen con NPM FilePond

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import * as FilePond from 'filepond';
+import 'filepond/dist/filepond.min.css';
 
 window.Alpine = Alpine;
+window.FilePond = FilePond;
 
 Alpine.start();
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -103,16 +103,20 @@
   </div>
 
   @push('styles')
-    <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet" />
-    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet" />
+    {{-- <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet" />
+    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet" /> --}}
   @endpush
 
   @push('scripts')
-    <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
-    <script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+    {{-- <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
+    <script src="https://unpkg.com/filepond/dist/filepond.js"></script> --}}
 
-    <script>
-      FilePond.registerPlugin(FilePondPluginImagePreview);
+    <script type="module">
+      // FilePond.registerPlugin(FilePondPluginImagePreview);
+
+// import pt_BR from 'filepond/locale/es-es.js';
+
+// FilePond.setOptions(pt_BR);
 
       const inputElement = document.querySelector('input[id="filePond"]');
       const pond = FilePond.create(inputElement);

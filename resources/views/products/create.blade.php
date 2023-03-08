ed3e66011a7d2300374cc80893eb00957915a554
Doris Morales <dmorales55@gmail.com>
2023-03-08 15:14:04 -0500

    Productos Imagen: Dropzone y Laravel Media Library

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -65,20 +65,27 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
 
     <script>
-      var uploadedDocumentMap = {}
+      let uploadedDocumentMap = {}
 
       Dropzone.options.documentDropzone = {
         url: '{{ route('products.storeMedia') }}',
         maxFilesize: 2, // MB
         addRemoveLinks: true,
-        acceptedFiles: ".jpeg,.jpg,.png,.gif.,pdf",
+        // acceptedFiles: ".jpeg,.jpg,.png,.gif.,pdf",
+        acceptedFiles: 'image/*',
+	      maxFiles: 1,
+        dictDefaultMessage: "<h3 class='sbold'>Suelte los archivos aquí o haga clic para cargar el documento<h3>",
+        dictRemoveFile:'Quitar',
+        // paramName: 'image',     // Cambiar 'file' por 'image'
         headers: {
           'X-CSRF-TOKEN': "{{ csrf_token() }}"
         },
+
         success: function(file, response) {
           $('form').append('<input type="hidden" name="photo[]" value="' + response.name + '">')
           uploadedDocumentMap[file.name] = response.name
         },
+
         removedfile: function(file) {
           file.previewElement.remove()
           var name = ''
@@ -89,6 +96,7 @@
           }
           $('form').find('input[name="photo[]"][value="' + name + '"]').remove()
         },
+        
         init: function() {
           @if (isset($photos))
             var files = {!! json_encode($photos) !!}

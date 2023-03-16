cefaaf08f1eecc9b12bd46c95f665989bc54fdc6
Doris Morales <dmorales55@gmail.com>
2023-03-16 13:48:45 -0500

    Articulos Imagen: Dropzone - Intervention Image

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import swal from 'sweetalert2';
 
-import Dropzone from "dropzone";
 import * as FilePond from 'filepond';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import pt_ES from 'filepond/locale/es-es.js';
@@ -23,36 +22,3 @@ window.FilePondPluginImagePreview = FilePondPluginImagePreview;
 Alpine.start();
 
 FilePond.setOptions(pt_ES);
-
-// Deshabilitar el comportamiento de detección automática
-Dropzone.autoDiscover = false;
-
-let dropzone = new Dropzone("#dropzone", {
-  paramName: "file",
-  acceptedFiles: 'image/*',
-  addRemoveLinks: true,
-  maxFiles: 1,
-  uploadMultiple: false,
-  dictDefaultMessage: "Suelte los archivos aquí o haga clic para cargar la imagen",
-  dictRemoveFile: "Quitar archivo",
-
-  init: function () {
-    if (document.querySelector('[name="image"]').value.trim()) { // si hay algo
-      let imagePublished = {}
-      imagePublished.size = 1234;
-      imagePublished.name = document.querySelector('[name="image"]').value;
-
-      this.options.addedfile.call(this, imagePublished);
-      this.options.thumbnail.call(this, imagePublished, "/uploads/" + imagePublished.name);
-      imagePublished.previewElement.classList.add("dz-success", "dz-complete");
-    }
-  }
-});
-
-dropzone.on('success', function (file, response) {
-  document.querySelector('[name="image"]').value = response.image;
-});
-
-dropzone.on('removedfile', function (file) {
-  document.querySelector('[name="image"]').value = "";
-});

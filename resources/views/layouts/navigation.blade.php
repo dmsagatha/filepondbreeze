58b00c5b8cd0edc5a59b9fbfbed27f10e75ef89d
Doris Morales <dmorales55@gmail.com>
2023-03-23 17:42:39 -0500

    Articulos: NPM Dropzone - Intervention Image

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,7 +19,7 @@
             {{ __('Posts') }} - Filepond
           </x-nav-link>
           <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
-            {{ __('Products') }} - Dropzone
+            {{ __('Products') }} - Media-Dropzone
           </x-nav-link>
           <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.index')">
             {{ __('Categories') }} - Dropzone

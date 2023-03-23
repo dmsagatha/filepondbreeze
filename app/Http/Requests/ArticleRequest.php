0ec3ebee9e852c12a0dcd7b16d2cd8463aa1d899
Doris Morales <dmorales55@gmail.com>
2023-03-23 15:29:01 -0500

    Articulos: NPM Dropzone - Intervention Image

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -24,7 +24,7 @@ class ArticleRequest extends FormRequest
     return [
       'name'        => $name,
       'description' => 'required',
-      'image'       => 'required',
+      'image'       => 'required'
     ];
   }
 }
